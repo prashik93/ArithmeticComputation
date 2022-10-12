50146c98704fbbffe229878d3adb4e0d43934b51
Prashik Kamble <kambleprashik7349@gmail.com>
2022-10-12 10:14:22 +0530

    [Prashik]Added code to perform first operation

--- arithmeticComputations.sh
+++ arithmeticComputations.sh
@@ -1,6 +1,9 @@
 #! /bin/bash/ -x
 
-read -p "Enter First Number :- " first
-read -p "Enter Second Number :- " second
-read -p "Enter Third Number :- " third
+read -p "Enter First Number :- " a
+read -p "Enter Second Number :- " b
+read -p "Enter Third Number :- " c
+
+firstOperation=$(($a + $b * $c))
+echo $firstOperation
 
