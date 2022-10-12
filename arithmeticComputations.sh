e6aba0dfa2ebc7ef0ecb00053ae1db3451c0546a
Prashik Kamble <kambleprashik7349@gmail.com>
2022-10-12 11:44:24 +0530

    [Prashik]Refactor the code to store computation result in a dictionary

--- arithmeticComputations.sh
+++ arithmeticComputations.sh
@@ -11,3 +11,40 @@ secondOperation=$(($a * $b + $c))
 thirdOperation=$(( $c + ($a/$b) ))
 
 fourthOperation=$(( ($a % $b) + $c ))
+
+
+firstOperationKey=1
+secondOperationKey=2
+thirdOperationKey=3
+fourthOperationKey=4
+
+declare -A resultDictionary
+
+flag=true
+
+while $flag
+do
+	if [ $firstOperation ]
+	then
+		resultDictionary[ $firstOperationKey ]=$firstOperation
+	fi
+
+	if [ $secondOperation ]
+	then
+		resultDictionary[ $secondOperationKey ]=$secondOperation
+	fi
+
+	if [ $thirdOperation ]
+	then
+		resultDictionary[ $thirdOperationKey ]=$thirdOperation
+	fi
+
+	if [ $fourthOperation ]
+	then
+		resultDictionary[ $fourthOperationKey ]=$fourthOperation
+	fi
+	flag=false
+done
+
+echo ${!resultDictionary[@]}
+echo ${resultDictionary[@]}
